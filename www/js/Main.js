c09646dafee1bf411e8483211a804ced19f14a11
Archil Imnadze <archil@imnadze.ge>
2017-04-25 20:56:32 +0400

    zoom

--- www/js/Main.js
+++ www/js/Main.js
@@ -13,9 +13,11 @@ var tileVisibleWidth = 69.28,
         g.setAttribute('transform', 'translate(' + (innerWidth * 0.5) + ', ' + (innerHeight * 0.5) + ')')
     }
 
+    var zoom = 4
+
     var tiles = []
-    for (var y = -20; y <= 20; y++) {
-        for (var x = -20; x <= 20; x++) {
+    for (var y = -3; y <= 3; y++) {
+        for (var x = -3; x <= 3; x++) {
             tiles.push(Tile(x, y))
         }
     }
@@ -24,16 +26,34 @@ var tileVisibleWidth = 69.28,
         return a.zIndex > b.zIndex ? 1 : -1
     })
 
-    var g = document.createElementNS(svg_xmlns, 'g')
+    var zoomG = document.createElementNS(svg_xmlns, 'g')
     tiles.forEach(function (tile) {
-        g.appendChild(tile.element)
+        zoomG.appendChild(tile.element)
     })
+
+    var g = document.createElementNS(svg_xmlns, 'g')
+    g.appendChild(zoomG)
 
     var svg = document.createElementNS(svg_xmlns, 'svg')
     svg.setAttribute('class', 'Main')
     svg.appendChild(g)
 
     document.body.appendChild(svg)
+
+    addEventListener('wheel', function (e) {
+        if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return
+        if (e.deltaY > 0) {
+            e.preventDefault()
+            if (zoom === 1) return
+            zoom /= 2
+            zoomG.setAttribute('transform', 'scale(' + zoom / 4 + ')')
+        } else if (e.deltaY < 0) {
+            e.preventDefault()
+            if (zoom === 16) return
+            zoom *= 2
+            zoomG.setAttribute('transform', 'scale(' + zoom / 4 + ')')
+        }
+    })
 
     addEventListener('resize', resize)
     resize()
--- www/js/Tile.js
+++ www/js/Tile.js
@@ -1,13 +1,23 @@
 function Tile (x, y) {
 
     var image = document.createElementNS(svg_xmlns, 'image')
+    image.setAttribute('class', 'Tile-image')
     image.setAttributeNS(xlink_xmlns, 'href', 'img/tile/grass.svg')
     image.setAttribute('width', tileWidth)
     image.setAttribute('height', tileHeight)
-    image.setAttribute('transform', 'translate(-' + (tileWidth * 0.5) + ', -' + tileHeight * 0.5 + ') translate(' + (x * tileVisibleWidth * 0.5 + y * tileVisibleWidth * 0.5) + ', ' + (y * tileVisibleHeight * 0.5 - x * tileVisibleHeight * 0.5) + ')')
+    image.setAttribute('transform', 'translate(-' + (tileWidth * 0.5) + ', -' + tileHeight * 0.5 + ')')
+
+    var maskPath = document.createElementNS(svg_xmlns, 'path')
+    maskPath.setAttribute('class', 'Tile-mask')
+    maskPath.setAttribute('d', 'M0 -20 L34.641 0 0 20 -34.641 0 Z')
+
+    var g = document.createElementNS(svg_xmlns, 'g')
+    g.appendChild(image)
+    g.appendChild(maskPath)
+    g.setAttribute('transform', 'translate(' + (x * tileVisibleWidth * 0.5 + y * tileVisibleWidth * 0.5) + ', ' + (y * tileVisibleHeight * 0.5 - x * tileVisibleHeight * 0.5) + ')')
 
     return {
-        element: image,
+        element: g,
         zIndex: y - x,
     }
 
