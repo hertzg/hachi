c525b4449808e5c0eff7195bf269d1e32332538f
Archil Imnadze <archil@imnadze.ge>
2017-04-26 15:28:33 +0400

    walls

--- www/js/Main.js
+++ www/js/Main.js
@@ -24,6 +24,12 @@ var buildingWidth = 160.00000,
         buildingsMap[(x + 1) + ',' + (y + 1)] = building
     }
 
+    function putObstacle (x, y, type) {
+        var obstacle = Obstacle(x, y, type)
+        obstacles.push(obstacle)
+        obstaclesMap[x + ',' + y] = obstacle
+    }
+
     function random (array) {
         return array[Math.floor(Math.random() * array.length)]
     }
@@ -50,29 +56,41 @@ var buildingWidth = 160.00000,
     }
 
     var zoom = 4
-    var mapSize = 5
+    var mapSize = 8
 
     var trees = ['tree', 'trees-1', 'trees-2', 'bush', 'bushes-1', 'bushes-2']
 
     var buildings = []
     var buildingsMap = Object.create(null)
-    putBuilding(-mapSize, -mapSize, 'castle')
-    putBuilding(mapSize - 1, mapSize - 1, 'castle')
-    putBuilding(1, 2, 'farm')
-    putBuilding(-2, -3, 'farm')
-    putBuilding(-4, 0, 'tower')
-    putBuilding(3, 0, 'tower')
-    putBuilding(-3, 3, 'gold')
+    putBuilding(-mapSize + 1, -mapSize + 1, 'castle')
+    putBuilding(mapSize - 2, mapSize - 2, 'castle')
+    putBuilding(mapSize - 5, mapSize - 4, 'farm')
+    putBuilding(-mapSize + 5, -mapSize + 3, 'farm')
+    putBuilding(-5, -1, 'tower')
+    putBuilding(4, 1, 'tower')
     putBuilding(3, -5, 'stone')
+    putBuilding(-3, 3, 'gold')
+
+    var obstacles = []
+    var obstaclesMap = Object.create(null)
+    putObstacle(-1, -8, 'wall-vertical')
+    putObstacle(-1, -7, 'wall-vertical')
+    putObstacle(-1, -6, 'wall-vertical')
+    putObstacle(-1, -5, 'wall-vertical')
+    putObstacle(-4, -2, 'wall-horizontal')
+    putObstacle(-5, -2, 'wall-horizontal')
+    putObstacle(-6, -2, 'wall-horizontal')
+    putObstacle(-7, -2, 'wall-horizontal')
+    putObstacle(-8, -2, 'wall-horizontal')
 
     var tiles = []
-    var obstacles = []
     for (var y = -mapSize; y <= mapSize; y++) {
         for (var x = -mapSize; x <= mapSize; x++) {
             var groundType = Math.random() < 0.7 ? 'grass' : 'gravel'
             var tile = Tile(x, y, groundType)
             tiles.push(tile)
-            if (buildingsMap[x + ',' + y] === undefined) {
+            if (buildingsMap[x + ',' + y] === undefined &&
+                obstaclesMap[x + ',' + y] === undefined) {
                 if (Math.random() < 0.2 && groundType === 'grass') {
                     obstacles.push(Obstacle(x, y, random(trees)))
                 }
