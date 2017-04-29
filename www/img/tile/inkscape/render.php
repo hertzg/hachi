1ba9492318252e2e8716772cb60f690e16136be3
Archil Imnadze <archil@imnadze.ge>
2017-04-29 10:42:43 +0400

    optimize frequent images to gain performance

---
+++ www/img/tile/inkscape/render.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?php
+
+chdir(__DIR__);
+echo __DIR__."\n";
+include_once '../../../fns/optimize_xml_file.php';
+foreach (glob('*.svg') as $file) {
+    echo "    $file\n";
+    system("inkscape --vacuum-defs $file");
+    system("inkscape --export-plain-svg=../$file $file");
+    optimize_xml_file("../$file");
+}
